53f47b6dfb30ac27a3b4b98d6ff79d513473160b
CR-Alejandro2905 <rojaspiedraalejandro29@gmail.com>
2025-04-17 21:58:30 -0600

    Usuarios / Roles / Correo

--- src/main/java/com/veganSitavi/ProjectConfig.java
+++ src/main/java/com/veganSitavi/ProjectConfig.java
@@ -1,10 +1,21 @@
 package com.veganSitavi;
 
+import com.veganSitavi.domain.Ruta;
+import com.veganSitavi.service.RutaPermitService;
+import com.veganSitavi.service.RutaService;
+import java.util.List;
 import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -51,33 +62,51 @@ public class ProjectConfig implements WebMvcConfigurer{
         return resolver;
     }
     
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
-//        http.authorizeHttpRequests((request) -> request.requestMatchers("/", "/index", "/error/**","/carrito/**",
-//                        "/reportes/**", "/registro/**", "/js/**", "/webjars/**")
-//                .permitAll()
-//                .requestMatchers("/producto/nuevo", "/producto/guardar/**","/producto/eliminar/**",
-//                        "/producto/modificar/**", "/categoria/nuevo/**","/categoria/guardar/**","/categoria/eliminar/**",
-//                        "/categoria/modificar/**")
-//                .hasRole("ADMIN")
-//                .requestMatchers("/producto/listado", "/categoria/listado","/pruebas/**")
-//                
-//                .hasRole("USER")).formLogin((form)-> form.loginPage("/login").permitAll())
-//                .logout((logout) -> logout.permitAll());
-//        return http.build();
-//    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login").setViewName("login");
+        registro.addViewController("/index").setViewName("index");
+        registro.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
+    
+    
+    @Autowired
+    private RutaPermitService rutaPermitService;
+    @Autowired
+    private RutaService rutaService;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        String[] rutasPermit = rutaPermitService.getRutaPermitsString();
+        List<Ruta> rutas = rutaService.getRutas();
+
+        http.authorizeHttpRequests((request) -> {
+            request
+                    .requestMatchers(rutasPermit).permitAll();
+            for (Ruta ruta : rutas) {
+                System.out.println(ruta.getPatron()+":"+ruta.getRolName());
+                request
+                        .requestMatchers(ruta.getPatron())
+                        .hasRole(ruta.getRolName());
+            }
+        }
+        )
+                .formLogin((form) -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/index", true).permitAll())
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
     
-//   @Bean
-//   public UserDetailsService users() {
-//       UserDetails admin = User.builder().username("leo").password("{noop}123")
-//               .roles("USER","ADMIN").build();
-//       UserDetails usuario = User.builder().username("pedro").password("{noop}123")
-//               .roles("USER").build();
-//       
-//       return new InMemoryUserDetailsManager(admin,usuario);
-//   }
-    
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
     
 }
 
--- src/main/java/com/veganSitavi/service/UsuarioDetailsService.java
+++ src/main/java/com/veganSitavi/service/UsuarioDetailsService.java
@@ -6,36 +6,36 @@ import com.veganSitavi.repository.UsuarioRepository;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-//@Service("userDetailsService")
-//public class UsuarioDetailsService implements UserDetailsService {
-//    @Autowired
-//    private UsuarioRepository usuarioRepository;
-//    @Autowired
-//    private HttpSession session;
+@Service("userDetailsService")
+public class UsuarioDetailsService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private HttpSession session;
 
-//    @Override
-//    @Transactional(readOnly = true)
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        //Busca el usuario por el username en la tabla
-//        Usuario usuario = usuarioRepository.findByUsername(username);
-//        //Si no existe el usuario lanza una excepción
-//        if (usuario == null) {
-//            throw new UsernameNotFoundException(username);
-//        }
-//        session.removeAttribute("usuarioImagen");
-//        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
-//        //Si está acá es porque existe el usuario... sacamos los roles que tiene
-//        var roles = new ArrayList<GrantedAuthority>();
-//        for (Rol rol : usuario.getRoles()) {   //Se sacan los roles
-//            roles.add(new SimpleGrantedAuthority("ROLE_"+rol.getNombre()));
-//        }
-//        //Se devuelve User (clase de userDetails)
-//        return new User(usuario.getUsername(), usuario.getPassword(), roles);
-//    }
-//}
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Busca el usuario por el username en la tabla
+        Usuario usuario = usuarioRepository.findByUsername(username);
+        //Si no existe el usuario lanza una excepción
+        if (usuario == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+        //Si está acá es porque existe el usuario... sacamos los roles que tiene
+        var roles = new ArrayList<GrantedAuthority>();
+        for (Rol rol : usuario.getRoles()) {   //Se sacan los roles
+            roles.add(new SimpleGrantedAuthority("ROLE_"+rol.getNombre()));
+        }
+        //Se devuelve User (clase de userDetails)
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
+    }
+}
