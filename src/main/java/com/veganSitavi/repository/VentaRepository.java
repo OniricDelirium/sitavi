8c5a1c47058221826f0d0f2750aa75cb3a6da69d
Leo <Leo@DESKTOP-J62O49J>
2025-04-20 14:23:32 -0600

    Carrito offcanva

---
+++ src/main/java/com/veganSitavi/repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package com.veganSitavi.repository;
+
+import com.veganSitavi.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository <Venta,Long> {
+     
+}
