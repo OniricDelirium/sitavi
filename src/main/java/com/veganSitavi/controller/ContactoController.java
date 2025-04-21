4ccc54e8f5d2cbc1a26c9cf53b88c5c814c7befa
CR-Alejandro2905 <rojaspiedraalejandro29@gmail.com>
2025-04-21 15:49:58 -0600

    Contacto

--- src/main/java/com/veganSitavi/controller/ContactoController.java
+++ src/main/java/com/veganSitavi/controller/ContactoController.java
@@ -1,15 +1,43 @@
 package com.veganSitavi.controller;
 
+import com.veganSitavi.domain.Contacto;
+import com.veganSitavi.service.CorreoService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/contacto")
 public class ContactoController {
 
+    @Autowired
+    private CorreoService correoService;
+
     @GetMapping("/formulario")
-    public String mostrarContacto() {
-        return "contacto/formulario"; 
+    public String mostrarContacto(Model model) {
+        model.addAttribute("contacto", new Contacto());
+        return "/contacto/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String procesarFormulario(@ModelAttribute Contacto contacto) throws MessagingException {
+        String destinatario = "rojaspiedraalejandro29@gmail.com";
+
+        String asunto = "Nuevo mensaje desde Vegan Sitavi";
+
+        String contenidoHtml = "<h2>Formulario de Contacto</h2>"
+                + "<p><strong>Nombre:</strong> " + contacto.getNombreCompleto() + "</p>"
+                + "<p><strong>Apellidos:</strong> " + contacto.getApellidos() + "</p>"
+                + "<p><strong>Correo:</strong> " + contacto.getCorreo() + "</p>"
+                + "<p><strong>Mensaje:</strong><br>" + contacto.getMensaje() + "</p>";
+
+        correoService.enviarCorreoHtml(destinatario, asunto, contenidoHtml);
+
+        return "redirect:/contacto/formulario";
     }
 }
---
+++ src/main/java/com/veganSitavi/domain/Contacto.java
@@ -0,0 +1,42 @@
+package com.veganSitavi.domain;
+
+public class Contacto {
+    private String nombreCompleto;
+    private String apellidos;
+    private String correo;
+    private String mensaje;
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
